783b76429b7a6daaaf225666793f2ec112457920
Dominguez Gaston Matias <gastonmd14@gmail.com>
2022-03-22 16:32:49 -0300

    update post for signup users

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -17,13 +17,17 @@ router.get('/signup', (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
     try {
         const user = req.body;
-        const result = handler.createUser(user);
-        if (result) {
-            res.status(200).json({message: 'User Created'});
+        const userToFind = await handler.findUser(user);
+        if(!userToFind){
+            const result = await handler.createUser(user);
+            if(result) {
+                res.status(200).json({message: 'User Created'});
+            }
+            res.status(500).json({message: 'Something Failed'});
         }
-        res.status(401).json({message: 'Something Failed'});
+        res.status(401).json({message: 'Email Already in Use'})
     } catch (err) {
-        res.status(500).json({message: err});
+        console.log(err);
     }
 });
 
